068e8d87c211a306504a7e056168d6b00a983f15
Jun <gjdeveloper@vers.kr>
2023-03-05 17:37:47 +0900

    About And OpenSource License

--- lib/features/setting/setting_screen.dart
+++ lib/features/setting/setting_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatelessWidget {
@@ -7,15 +6,31 @@ class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Settings"),
-        ),
-        body: Column(
-          children: [
-            CupertinoActivityIndicator(),
-            CircularProgressIndicator(),
-            CircularProgressIndicator.adaptive(),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            // onTap: () => showAboutDialog(
+            //   context: context,
+            //   applicationVersion: "1.0",
+            //   applicationLegalese: "이 앱은 클론 코딩 앱입니다.",
+            // ),
+            onTap: () => showLicensePage(
+              context: context,
+            ),
+            title: Text(
+              "About",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: Text("About this app"),
+          ),
+          AboutListTile(),
+        ],
+      ),
+    );
   }
 }
