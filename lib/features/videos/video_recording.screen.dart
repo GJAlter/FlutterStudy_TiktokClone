687a8b50f2c6cdc0edd9a041063d30c53d997ce3
Jun <gjdeveloper@vers.kr>
2023-03-12 22:06:19 +0900

    Camera FlashMode

--- lib/features/videos/video_recording.screen.dart
+++ lib/features/videos/video_recording.screen.dart
@@ -12,9 +12,25 @@ class VideoRecordingScreen extends StatefulWidget {
 }
 
 class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
-  late final CameraController _cameraController;
+  late CameraController _cameraController;
 
   bool _isGrantedPermissions = false;
+  bool _isSelfieMode = false;
+  int _currentFlashMode = 0;
+
+  final List<FlashMode> _flashModes = [
+    FlashMode.off,
+    FlashMode.always,
+    FlashMode.auto,
+    FlashMode.torch,
+  ];
+
+  final List<IconData> _flashModeIcons = [
+    Icons.flash_off_rounded,
+    Icons.flash_on_rounded,
+    Icons.flash_auto_rounded,
+    Icons.flashlight_on_rounded,
+  ];
 
   Future<void> initPermissions() async {
     final cameraPermission = await Permission.camera.request();
@@ -33,9 +49,25 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   Future<void> initCamera() async {
     final cameras = await availableCameras();
     if (cameras.isEmpty) return;
-    _cameraController = CameraController(cameras[1], ResolutionPreset.ultraHigh);
+    _cameraController = CameraController(cameras[_isSelfieMode ? 1 : 0], ResolutionPreset.max);
 
     await _cameraController.initialize();
+    _currentFlashMode = _flashModes.indexOf(_cameraController.value.flashMode);
+    setState(() {});
+  }
+
+  Future<void> _changeCameraMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    await initCamera();
+  }
+
+  Future<void> _changeFlashMode() async {
+    if (_currentFlashMode == _flashModes.length - 1) {
+      _currentFlashMode = 0;
+    } else {
+      _currentFlashMode++;
+    }
+    await _cameraController.setFlashMode(_flashModes[_currentFlashMode]);
     setState(() {});
   }
 
@@ -48,30 +80,55 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SizedBox(
-      width: MediaQuery.of(context).size.width,
-      child: !_isGrantedPermissions || !_cameraController.value.isInitialized
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  "Initializing...",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: Sizes.size20,
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: !_isGrantedPermissions || !_cameraController.value.isInitialized
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Initializing...",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: Sizes.size20,
+                    ),
                   ),
-                ),
-                Gaps.v20,
-                CircularProgressIndicator.adaptive(),
-              ],
-            )
-          : Stack(
-              alignment: Alignment.center,
-              children: [
-                CameraPreview(_cameraController),
-              ],
-            ),
-    ));
+                  Gaps.v20,
+                  CircularProgressIndicator.adaptive(),
+                ],
+              )
+            : Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(_cameraController),
+                  Positioned(
+                    top: Sizes.size28,
+                    right: 0,
+                    child: Column(
+                      children: [
+                        IconButton(
+                          color: Colors.white,
+                          onPressed: _changeCameraMode,
+                          icon: const Icon(
+                            Icons.cameraswitch,
+                          ),
+                        ),
+                        Gaps.v10,
+                        IconButton(
+                          color: _flashModes[_currentFlashMode] == FlashMode.off ? Colors.white : Colors.amber.shade200,
+                          onPressed: _changeFlashMode,
+                          icon: Icon(
+                            _flashModeIcons[_currentFlashMode],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
   }
 }
